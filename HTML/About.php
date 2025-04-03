11d873f22629a0fc81017269a0797e734fa8adcd
darienrh <ramirezhennesseydarien@gmail.com>
2025-04-03 14:50:45 -0400

    edits

--- HTML/About.php
+++ HTML/About.php
@@ -19,7 +19,7 @@
                 <p>Front End Designer</p>
             </div>
             <div class="member">
-                <img src="/images/blank-profile-picture.png" alt="Darien">
+                <img src="blank-profile-picture.png" alt="Darien">
                 <h2>Darien Ramirez-Hennessey</h2>
                 <p>Back End Developer</p>
             </div>
