f585a954f56d048ecd5f21784e6ef2b03d0a3330
darienrh <darien.ramirez-hennessey@mths.ca>
2025-04-08 16:55:59 -0400

    should get the sql to work maybe

--- HTML/logout.php
+++ HTML/logout.php
@@ -3,5 +3,5 @@ session_start();        // Start the session
 session_unset();        // Unset all session variables
 session_destroy();      // Destroy the session
 
-header("Location: LoginPage.php"); // Redirect to login or homepage
+header("Location: LandingPage.php"); // Redirect to login or homepage
 exit();
